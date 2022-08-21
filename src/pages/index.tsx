8288bf05270842d959ece0cb0ab28a11ac4f14e2
aulb <aalbertuntung@gmail.com>
2022-08-21 00:03:35 -0700

    Pre-build, overwriting old stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,12 @@
 import * as React from "react";
 import Layout from "../layouts";
-import styled from "styled-components";
-
-const Heading = styled.h1`
-  marginTop: 0;
-  marginBottom: 24;
-`;
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="my webpage">
-      <Heading>albert's ウェッブページ</Heading>
+    <Layout pageTitle="アルベルトのサイト">
       <p>
-        ヘイ!albertです。現在、言語を学ぶことが興味深いです。
+        hi! i'm albert.お気軽にお問い合わせくださいませ⬇️
       </p>
-      <h2>ブログ</h2>
     </Layout>
   )
 };
