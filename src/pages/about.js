41a7848f897964bf68427f2016ab09c3a6320e5d
albert <aalbertuntung@gmail.com>
2023-01-13 16:33:02 -0800

    Comitting so I can work on my laptop
    None.

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export const Head = () => (
+  <>
+    <title>About Me</title>
+    <meta name="description" content="Your description" />
+  </>
+)
+
+export default AboutPage
