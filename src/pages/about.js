0468f745e93f25e492623260b0fb469df0ae7cf3
albert <aalbertuntung@gmail.com>
2023-01-13 17:10:27 -0800

    Part 2 Gatsby tutorial done
    Finished part 2 Gatsby tutorial.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,11 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
-  )
+      <Layout pageTitle="Home Page">
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+      </Layout>
+    )
 }
 
 export const Head = () => <title>Home Page</title>
