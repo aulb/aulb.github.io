cdcee82fc6426c6df91846379031ea4661386a35
albert <aalbertuntung@gmail.com>
2023-01-16 21:28:20 -0800

    Finished part 6
    Title.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
   return (
@@ -10,7 +10,11 @@ const BlogPage = ({ data }) => {
       {
         data?.allMdx?.nodes?.map((node) => (
           <article key={node?.id}>
-            <h2>{node?.frontmatter?.title}</h2>
+            <h2>
+              <Link to={`/blog/${node?.frontmatter?.test}`}>
+                {node?.frontmatter?.title}
+              </Link>
+            </h2>
             <p>Posted: {node?.frontmatter?.date}</p>
             <p>{node?.excerpt}</p>
           </article>
@@ -30,6 +34,8 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
+          test
         }
       }
     }
