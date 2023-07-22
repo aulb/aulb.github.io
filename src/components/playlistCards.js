092be7920ff56e6442d1ded4bcab6e78e8799437
albert <aalbertuntung@gmail.com>
2023-07-21 23:09:49 -0700

    Cleaned cards using inline-flex.

--- src/components/playlistCards.js
+++ src/components/playlistCards.js
@@ -9,7 +9,6 @@ import {
 } from "./playlistCards.module.css"
 import {useState} from "react";
 
-const MAX_CARDS = 4
 const sortPlaylistByMostRecent = (playlistA, playlistB) => {
   const createdAtA = new Date(playlistA.created_at) 
   const createdAtB = new Date(playlistB.created_at) 
@@ -18,22 +17,35 @@ const sortPlaylistByMostRecent = (playlistA, playlistB) => {
   return 0
 }
 
-const PlaylistCards = () => {
+const PlaylistCards = ({ 
+  maxCard = 4, 
+  includeHeader = true, 
+  includeShowAll = true,
+}) => {
   const [shouldShowAll, setShouldShowAll] = useState(false)
   const playlists = SpotifyJSONData.items
     .filter(playlist => playlist.public)
     .sort(sortPlaylistByMostRecent)
-    .slice(0, shouldShowAll ? SpotifyJSONData.items.length : MAX_CARDS)
+    .slice(0, shouldShowAll ? SpotifyJSONData.items.length : maxCard)
+  const shouldRenderShowAllContainer = includeHeader || includeShowAll
   return (
     <div className={mainContainer}>
-      <div className={showAllContainer}>
-        <div onClick={() => setShouldShowAll(!shouldShowAll)} className={showAllText}>
-          Show all
-        </div>
-        <div>
-          <strong>My monthly playlist</strong>
-        </div>
-      </div>
+      {
+        shouldRenderShowAllContainer ?
+          <div className={showAllContainer}>
+            { includeShowAll ?
+                <div onClick={() => setShouldShowAll(!shouldShowAll)} className={showAllText}>
+                  Show all
+                </div> : <div></div>
+            }
+            {
+              includeHeader ?
+                <div>
+                  <strong>My monthly playlist</strong>
+                </div> : <div></div>
+            }
+          </div> : null
+      }
       <div className={cardContainer}>
         {playlists.map((playlist, index) => <PlaylistCard playlist={playlist} index={index} />)}
       </div>
