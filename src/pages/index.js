1e9798c18002d51892bd76979d31d99734607368
albert <aalbertuntung@gmail.com>
2023-07-26 16:48:56 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
       <Layout>
         <p>📍 Software engineer based in San Francisco </p>
         <p>👀 <Link to="https://www.usajapan.org/japanese-language-classes/japanese-group-classes/" target="_blank">Japanese</Link>, <Link to="https://www.clayroomsf.com/" target="_blank">studio pottery</Link>, light reading, <Link to="http://www.churchof8wheels.com/" target="_blank">rollerblading</Link>, and coffee</p>
-        <p>🎵 My recent <Link to="/playlists"><strong>monthly Spotify</strong></Link>'s genres visualized:</p>
+        <p>🎵 My recent <Link to="/playlists"><strong>monthly Spotify playlists</strong></Link>:</p>
         <PlaylistCards maxCard={2} includeHeader={false} includeShowAll={false} />
       </Layout>
     )
