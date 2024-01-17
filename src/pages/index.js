e802d383d34f3dffa11fe1682c53f0b63fa70ab4
albert <aalbertuntung@gmail.com>
2024-01-16 23:21:41 -0800

    Small update to about me

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,14 @@ const IndexPage = () => {
   return (
       <Layout>
         <p>📍 San Francisco based Software Engineer. Welcome to my page!</p>
-        <p>❤️ Some things I like:</p>
         <ul>
-          <li><Link to="http://nationalpokedex.github.io/" target="_blank">National Pokédex</Link></li>
           <li>
             <Link to="https://www.usajapan.org/japanese-language-classes/japanese-group-classes/" target="_blank">Japanese</Link>,
             <Link to="https://www.clayroomsf.com/" target="_blank"> pottery</Link>,
             <Link to="http://www.churchof8wheels.com/" target="_blank"> rollerblading</Link> in San Francisco
           </li>
           <li><Link to="https://www.recurse.com/" target="_blank">Recurse Center</Link>, programming sabatical in NYC</li>
+          <li><Link to="http://nationalpokedex.github.io/" target="_blank">Compact National Pokédex</Link> I built</li>
           <li><Link to="/maps" target="_blank">My maps!</Link> <Link to="https://www.google.com/maps/d/u/0/edit?mid=1a4om7EQLl9LV81wKUDGWtsgw3ZoJnnk&usp=sharing" target="_blank">Food recommendations</Link> in San Francisco</li>
         </ul>
         <p>🎵 My recent <Link to="/playlists" target="_blank"><strong>monthly Spotify playlists</strong></Link>:</p>
