4b01553bced1b67055025672d89dc9b6936d2c82
albert <aalbertuntung@gmail.com>
2024-09-13 16:15:24 -0700

    update
    simplify

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,19 +4,27 @@ import { Link } from 'gatsby'
 
 const Footer = () => {
   const data = useSiteMetadata()
-  const { github, instagram, linkedin, goodreads } = data
+  const { github, instagram, goodreads } = data
   return (
-    <div>
-      <Link to={github} target="_blank">github</Link>
-      &nbsp;•&nbsp;
-      <Link to={instagram} target="_blank">instagram</Link>
-      &nbsp;•&nbsp;
-      <Link to={linkedin} target="_blank">linkedin</Link>
-      &nbsp;•&nbsp;
-      <Link to={goodreads} target="_blank">goodreads</Link>
-      &nbsp;•&nbsp;
-      <Link to="/copyright">&copy;</Link> 2014 - {(new Date()).getFullYear()}
+    <>
+      <div>
+          <Link to="https://www.google.com/maps/d/u/0/edit?mid=1a4om7EQLl9LV81wKUDGWtsgw3ZoJnnk&usp=sharing" target="_blank">🍽️ recs</Link>&nbsp;•&nbsp;
+          <Link to="https://www.usajapan.org/japanese-language-classes/japanese-group-classes/" target="_blank">言語クラス</Link>&nbsp;•&nbsp;
+          <Link to="https://www.clayroomsf.com/" target="_blank">pottery</Link>&nbsp;•&nbsp;
+          <Link to="http://www.churchof8wheels.com/" target="_blank">rollerblading</Link>&nbsp;•&nbsp;
+          <Link to="https://www.recurse.com/" target="_blank">Recurse Center</Link>&nbsp;•&nbsp;
+          <Link to="http://nationalpokedex.github.io/" target="_blank">Pokédex</Link>
       </div>
+      <div>
+        <Link to={github} target="_blank">github</Link>
+        &nbsp;•&nbsp;
+        <Link to={instagram} target="_blank">ig</Link>
+        &nbsp;•&nbsp;
+        <Link to={goodreads} target="_blank">goodreads</Link>
+        &nbsp;•&nbsp;
+        <Link to="/copyright">&copy;</Link> 2014 - {(new Date()).getFullYear()}
+      </div>
+    </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,19 +7,8 @@ import { Link } from 'gatsby'
 const IndexPage = () => {
   return (
       <Layout>
-        <p>📍 San Francisco based Software Engineer. Welcome to my page!</p>
-        <ul>
-          <li>
-            <Link to="https://www.usajapan.org/japanese-language-classes/japanese-group-classes/" target="_blank">Japanese</Link>,
-            <Link to="https://www.clayroomsf.com/" target="_blank"> pottery</Link>,
-            <Link to="http://www.churchof8wheels.com/" target="_blank"> rollerblading</Link> in San Francisco
-          </li>
-          <li><Link to="https://www.recurse.com/" target="_blank">Recurse Center</Link>, programming sabatical in NYC</li>
-          <li><Link to="http://nationalpokedex.github.io/" target="_blank">Compact National Pokédex</Link> I built</li>
-          <li><Link to="/maps" target="_blank">My maps!</Link> <Link to="https://www.google.com/maps/d/u/0/edit?mid=1a4om7EQLl9LV81wKUDGWtsgw3ZoJnnk&usp=sharing" target="_blank">Food recommendations</Link> in San Francisco</li>
-        </ul>
-        <p>🎵 My recent <Link to="/playlists" target="_blank"><strong>monthly Spotify playlists</strong></Link>:</p>
-        <PlaylistCards maxCard={2} includeHeader={false} includeShowAll={false} />
+        <p>Software Engineer in San Francisco  🌉</p>
+        <PlaylistCards includeHeader={false} includeShowAll={true} />
       </Layout>
     )
 }
