6a1691b7a30f419f579ee908db4f48bb361be341
albert <aalbertuntung@gmail.com>
2024-09-13 16:29:33 -0700

    move everything to footer

---
+++ component---src-pages-index-js-94a0bd97079f4ef33a5c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkalbert_wtf=self.webpackChunkalbert_wtf||[]).push([[678],{6558:function(e,t,n){n.r(t),n.d(t,{Head:function(){return u}});var l=n(7294),a=n(8678),r=n(9357),c=n(3858);n(1883);const u=()=>l.createElement(l.Fragment,null,l.createElement(r.Z,null),l.createElement("meta",{name:"description",content:"albert.wtf"}));t.default=()=>l.createElement(a.Z,null,l.createElement(c.Z,{includeHeader:!1,includeShowAll:!0}))}}]);
+//# sourceMappingURL=component---src-pages-index-js-94a0bd97079f4ef33a5c.js.map
